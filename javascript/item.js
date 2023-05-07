06d7895bd0815d7d79fbd64410a8669edea3a01e
Horaciomdp <horazopo@gmail.com>
2023-05-07 15:40:42 -0300

    correccion error negativo

--- javascript/item.js
+++ javascript/item.js
@@ -1,11 +1,12 @@
-
+/*inicio seccion shop que realiza la suma de productos*/
 function sumar(){
     let num = document.getElementById('num').value;
     
     if(num == ''){
         num ="0";
+        
     }
-   
+    
     let sumar = parseInt(num) +1;
     document.getElementById('num').value = sumar;
 }
@@ -13,11 +14,15 @@ function sumar(){
 function restar(){
     let num = document.getElementById('num').value;
     
-    if(num == ''){
+    if(num == '' || num == 0){
         num ="0";
+        alert("No tiene items para quitar"); 
     }
    
-    let restar = parseInt(num) -1;
-    document.getElementById('num').value = restar;
+    if(num > "0"){
+        let restar = parseInt(num) -1;
+        document.getElementById('num').value = restar;
+    }
 }
 
+/*fin seccion shop que realiza la suma de productos*/
